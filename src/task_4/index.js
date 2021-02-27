45ca576c8388849d6941ecdf28feadea33beec6e
Anton Dobrynin <andeedobro@gmail.com>
2021-02-27 16:54:34 +0500

    Добрынин Антон - Четвертая задача

--- src/task_4/index.js
+++ src/task_4/index.js
@@ -13,6 +13,40 @@
 @returns {number} Количество удаленных записей
  */
 function findAndRemove(phoneBook, query) {
+	if (query === '') return 0;
+	if (query === '*') {
+		let removedItemsCount = phoneBook.length;
+		phoneBook.splice(0, removedItemsCount);
+		return removedItemsCount; 
+	}
+	
+	let removedItemsCount = 0;
+	for (let i = 0; i < phoneBook.length; i++) {
+		const note = phoneBook[i];
+
+		if (parsePhoneNumberDash(note.phone).includes(query) || parsePhoneNumberNoDash(note.phone).includes(query) || note.name.includes(query) 
+			|| (note.email != null && note.email.includes(query))) {
+			removedItemsCount++;
+			phoneBook.splice(i, 1);
+			i--;
+		}
+	}
+	
+	return removedItemsCount;
 }
+
+function parsePhoneNumberDash(phone) {
+	return !phone.includes("-") ? `+7-${phone.substr(2,3)}-${phone.substr(5,3)}-${phone.substr(8,2)}-${phone.substr(10,2)}` : phone;
+}
+
+function parsePhoneNumberNoDash(phone) {
+	const parts = phone.split("-");
+	return parts.length === 1 ? phone : parts[0] + parts[1] + parts[2] + parts[3] + parts[4];
+}
+
+
+const phoneBook = [{phone: '+7-800-555-35-35', name: 'Андрей', email: 'andrey@mail.ru'}, {phone: '+79225553535', name: 'Роман'}];
+console.log(findAndRemove(phoneBook, ''));
+console.log(phoneBook);
 
 module.exports.findAndRemove = findAndRemove;
