2b13a5b77b7d4c00bdbcdea633ad1b886f8f2701
NastyaKatyushkina <nastyusha_kat@mail.ru>
2021-03-03 23:40:37 +0500

    Катюшкина 1-2

--- src/task_1/index.js
+++ src/task_1/index.js
@@ -46,10 +46,11 @@ function CorrectPhone(phone)
 		{
 			return phone;
 		}
-		if (phone.match(/\+7\-\d{3}\-\d{3}\-\d{2}\-d{2}/) !== null)
+		if (phone.match(/\+7-\d{3}-\d{3}-\d{2}-d{2}/) !== null)
 		{
 			return phone.replace(/-/g, '');
 		}
+		return null;
 	}
 
 module.exports.add = add;
--- src/task_2/index.js
+++ src/task_2/index.js
@@ -24,10 +24,12 @@ function CorrectPhone(phone)
 		{
 			return phone;
 		}
-		if (phone.match(/\+7\-\d{3}\-\d{3}\-\d{2}\-d{2}/) !== null)
+		if (phone.match(/\+7-\d{3}-\d{3}-\d{2}-d{2}/) !== null)
 		{
 			return phone.replace(/-/g, '');
 		}
+		return null;
+	}
 }
 function update(phoneBook, phone, name, email) 
 {
