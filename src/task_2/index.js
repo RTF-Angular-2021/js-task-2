2c440594a541be450abaedbe9cd69b712b83a259
petr.diukin <petr.diukin@urfu.me>
2021-03-04 22:19:51 +0500

    Дюкин Петр

--- src/task_2/index.js
+++ src/task_2/index.js
@@ -1,24 +1,17 @@
-/** Задача 2 - Функция update
-Требуется написать функцию update, которая
-принимает: 
-	1) Текущее состояние телефонной книги
-	2) Номер телефон
-	3) Имя
-	4) Электронную почту,
-требуется:
-в текущем состояние телефонной книги обновить контакт по номеру телефона
-	1) Электронную почту можно стереть, а имя нет
-	2) Правила валидации полей такое же, как и при добавлении
-возвращает:
-	true - если обновление прошло успешно
-	false - если запись не обновилась
-@param {Array<{ phone: string, name: string, email?: string }>} phoneBook - Текущее состояние телефонной книги
-@param {string} phone Номер телефона
-@param {string} name Имя
-@param {string} email Электронная почта
-@returns {boolean} Результат обновления
- */
 function update(phoneBook, phone, name, email) {
+    let firstPattern = /^\+7-\d\d\d-\d\d\d-\d\d-\d\d$/;
+    let secondPattern = /^\+7\d\d\d\d\d\d\d\d\d\d$/;
+    if ((firstPattern.test(phone) || secondPattern.test(phone)) && name !== undefined && name !== '')
+    {
+        let number = phone.match(/\d/g).join('');
+        for (let i = 0; i < phoneBook.length; i++) {
+            if (phoneBook[i].phone.match(/\d/g).join('') !== number) continue;
+            phoneBook[i].name = name;
+            phoneBook[i].email = email;
+            return true;
+        }
+    }
+    return false;
 }
 
 module.exports.update = update;
